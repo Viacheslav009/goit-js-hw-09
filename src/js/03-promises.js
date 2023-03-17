11637db14516229a58535d751a94086c6e504b82
Viacheslav009 <mstislavpushkov@gmail.com>
2023-03-17 20:15:23 +0200

    all work

---
+++ src/js/03-promises.js
@@ -0,0 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const option = { useIcon: false };
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.delay.value);
+  const step = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          option
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, option);
+      });
+    delay += step;
+  }
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const objectPromise = { position, delay };
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(objectPromise);
+      }
+      reject(objectPromise);
+    }, delay);
+  });
+}
