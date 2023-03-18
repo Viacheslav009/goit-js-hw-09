9614294ac5465cfe57a701c60dae47e1a2cc2c2b
Viacheslav009 <114593269+Viacheslav009@users.noreply.github.com>
2023-03-18 20:22:27 +0000

    Deploying to gh-pages from @ Viacheslav009/goit-js-hw-09@0ffb029cab6a90f6d1252a9c603c63431782ebce 🚀

---
+++ 01-color-switcher.4d89bde9.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=null;e.setAttribute("disabled",!0),t.addEventListener("click",(function(){r=setInterval((function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled",!0),document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.4d89bde9.js.map
